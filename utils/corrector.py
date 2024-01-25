22200459887678dbd578ca360bfea1c4dc78b01a
ryosyntaxx <riodwi12174@gmail.com>
2024-01-25 17:15:17 +0700

    add corrector

---
+++ utils/corrector.py
@@ -0,0 +1,5 @@
+def vname(name: str) -> str:
+    invalid_chars = ['/', '\\', ':', '*', '?', '"', '<', '>', '|', '+', '=', '&', '%', '@', '#', '$', '^', '[', ']', '{', '}', '`', '~']
+    falid = ''.join(char if char not in invalid_chars else '' for char in name)
+    
+    return falid.replace(" ", "_").replace('__', '_')
