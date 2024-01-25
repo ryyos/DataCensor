2a37f0dd6a3cb7dd6c7621a2a23d969358e6a8ba
ryosyntaxx <riodwi12174@gmail.com>
2024-01-25 17:33:15 +0700

    add directory

---
+++ utils/directory.py
@@ -0,0 +1,15 @@
+import os
+from corrector import vname
+
+def create_dir(main_path: str, result: dict) -> str:
+    try: os.makedirs(f'{main_path}/data_raw/uptodown/{result["detail_application"]["platform"]}/{result["type"]}/{vname(result["reviews_name"].lower())}/json/detail')
+    except Exception: ...
+    finally: return f'{main_path}/data_raw/uptodown/{result["detail_application"]["platform"]}/{result["type"]}/{vname(result["reviews_name"].lower())}/json'
+    ...
+
+def convert_path(path: str) -> str:
+    
+    path = path.split('/')
+    path[1] = 'data_clean'
+    return '/'.join(path)
+    ...
