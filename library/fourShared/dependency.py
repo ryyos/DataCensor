ce82c1a3fc6397b48e332aab94fec8dc8317ffa1
ryosyntaxx <riodwi12174@gmail.com>
2024-02-08 23:45:12 +0700

    style: move extract to service

--- library/fourShared/dependency.py
+++ library/fourShared/dependency.py
@@ -1,18 +1,24 @@
+import os
 
-from typing import Dict, Tuple
+from typing import Dict, Tuple, List
 from ApiRetrys import ApiRetry
 from requests import Response
 from pyquery import PyQuery
+from concurrent.futures import ThreadPoolExecutor
 from icecream import ic
+from dekimashita import Dekimashita
 
 from components import FourSharedAsset
 from utils import *
 
 class FourSharedLibs(FourSharedAsset):
-    def __init__(self) -> None:
+    def __init__(self, save: bool) -> None:
         super().__init__()
 
         self.api = ApiRetry(show_logs=True)
+        self.executor = ThreadPoolExecutor()
+
+        self.SAVE_TO_LOKAL = save
 
     def extract_navbar(self, html: PyQuery) -> Tuple[str]:
         (size, posted, types, _) = html.find('p.fileInfo').text().split(' |')
@@ -20,33 +26,24 @@ class FourSharedLibs(FourSharedAsset):
         return (' '.join(size.split(' ')[1:]), posted.strip(), types.strip())
         ...
 
+    def create_dir(self, title: str) -> str:
 
-    def extract_header(self, url: str) -> Dict[str, any]:
+        format = title.split('.')[-1]
+        path = f'data/data_raw/admiralty/four_shared/{format}/{Dekimashita.vdir(title)}.{format}'
+        
+        try:
+            if self.SAVE_TO_LOKAL: os.makedirs(path)
+        except Exception: ...
+        finally: return path
+        ...
+
+    def collect_document(self, url: str) -> List[str]:
         response: Response = self.api.get(url=url, headers=self.headers, cookies=self.cookies)
         html = PyQuery(response.text)
 
-        (size, posted, types) = self.extract_navbar(html)
-
-        headers = {
-            "link": self.link,
-            "domain": self.domain,
-            "tag": [PyQuery(tag).text() for tag in html.find('#tagsDiv a')] + [self.domain],
-            "crawling_time": now(),
-            "crawling_time_epoch": epoch(),
-            "path_data_raw": "string",
-            "path_data_clean": "string",
-            "detail": {
-                "title": html.find('h1.fileName').text(),
-                "owner": html.find('a.fileOwner').text(),
-                "size": size,
-                "posted": posted,
-                "type": types,
-                "description": html.find('#fileDescriptionText').text()
-            }
-        }
-
-        ic(headers)
+        return [PyQuery(url).attr('href') for url in html.find('a[target="_top"]')]
         ...
+
     
 
 
--- services/fourShared/FourShared.py
+++ services/fourShared/FourShared.py
@@ -1,15 +1,53 @@
 import requests
 from urllib import request
 
+from requests import Response
+from pyquery import PyQuery
+from typing import Dict, Tuple
 from library import FourSharedLibs
 from icecream import ic
 
+from utils import *
+
 class FourShared(FourSharedLibs):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, save: bool) -> None:
+        super().__init__(save)
         ...
 
+
+    def extract(self, url: str, item: int = 0) -> None:
+        response: Response = self.api.get(url=url, headers=self.headers, cookies=self.cookies)
+        html = PyQuery(response.text)
+
+        (size, posted, types) = self.extract_navbar(html)
+
+        path = self.create_dir(title=html.find('h1.fileName').text())
+
+        headers = {
+            "link": self.link,
+            "domain": self.domain,
+            "tag": [PyQuery(tag).text() for tag in html.find('#tagsDiv a')] + [self.domain],
+            "crawling_time": now(),
+            "crawling_time_epoch": epoch(),
+            "path_data_raw": path,
+            "path_data_clean": convert_path(path),
+            "detail": {
+                "title": html.find('h1.fileName').text(),
+                "owner": html.find('a.fileOwner').text(),
+                "size": size,
+                "posted": posted,
+                "type": types,
+                "description": html.find('#fileDescriptionText').text()
+            },
+            "documents": self.collect_document(html.find('#moreFilesIFrame').attr('src'))
+        }
+
+        File.write_json
+        
+        ...
     
     def main(self) -> None:
-        self.extract_header(self.link)
+        self.extract(self.link)
+
+
         ...
