07c9132d692e4c5378d062a2655220a441e17065
ryosyntaxx <riodwi12174@gmail.com>
2024-02-11 21:50:05 +0700

    feat: add upload file in s3

--- playground.py
+++ playground.py
@@ -1,50 +1,5 @@
-import requests
+from share import FourSharedShere
 
-cookies = {
-    'hostid': '27532044',
-    '_ga': 'GA1.2.1594841001.1707297895',
-    '__utmz': '210074320.1707297936.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)',
-    'day1host': 'h',
-    'cd1v': 'PlT5',
-    '_gid': 'GA1.2.464384038.1707476935',
-    'Login': '1531194522',
-    'Password': '8a85738e0f6d41d3632e4f86a9c08c4e',
-    'ulin': 'true',
-    'JSESSIONID': 'B0F503AF3937BE995ECE41905E25FC65.dc573',
-    '4langcookie': 'in',
-    'WWW_JSESSIONID': 'B0F503AF3937BE995ECE41905E25FC65.dc573',
-    'dlpvc110439': 'N',
-    '__utma': '210074320.1594841001.1707297895.1707300404.1707476997.3',
-    '__utmc': '210074320',
-    '__utmt_gwo': '1',
-    'utrf': '9f5cf7b041',
-    '__utmb': '210074320.2.10.1707476997',
-}
-
-headers = {
-    'Accept': '*/*',
-    'Accept-Language': 'en-US,en;q=0.9,id;q=0.8',
-    'Connection': 'keep-alive',
-    'Referer': 'https://www.4shared.com/get/1VEFZFf8/Islam_Denounces_Terrorism.html',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
-    'X-Requested-With': 'XMLHttpRequest',
-    'locale': 'in',
-    'sec-ch-ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'x-security': '1531194522',
-}
-
-response = requests.get(
-    'https://dc601.4shared.com/download/1VEFZFf8/Islam_Denounces_Terrorism.pdf?tsid=20240209-111041-80007ed1&sbsr=d4027846b9421e10e9fff025d495dfdbb07&bip=MTE0LjEyNC4yNDAuMTM0&lgfp=30',
-    cookies=cookies,
-    headers=headers,
-)
-
-print(response)
-
-with open('private/test2.pdf', 'wb') as f:
-    f.write(response.content)
+if __name__== '__main__':
+    share = FourSharedShere()
+    share.main()
--- server/s3.py
+++ server/s3.py
@@ -2,6 +2,7 @@ import os
 import boto3
 
 from botocore.config import Config
+
 from dotenv import *
 from json import dumps
 from icecream import ic
@@ -20,13 +21,30 @@ class ConnectionS3:
                                  endpoint_url=endpoint_url,
                                 #  config=self.config
                                  )
+        
+        self.__s3_resource = boto3.resource('s3', 
+                                endpoint_url=endpoint_url,
+                                aws_access_key_id = access_key_id,
+                                aws_secret_access_key = secret_access_key
+                            ) 
 
     def upload(self, key: str, body: dict, bucket: str) -> int:
         response: dict = self.__s3.put_object(Bucket=bucket, Key=key, Body=dumps(body, indent=2, ensure_ascii=False))
+        self.__s3
         
         Runtime.s3(bucket, response['ResponseMetadata']['HTTPStatusCode'])
         return response['ResponseMetadata']['HTTPStatusCode']
         # return 400
+    
+    def upload_file(self, path: str, bucket: str, key: str) -> int:
+        response: dict = self.__s3_resource.Bucket(bucket).put_object(
+                                Key = key, 
+                                Body = open(path, 'rb')
+                            )
+        Runtime.s3(bucket, response['ResponseMetadata']['HTTPStatusCode'])
+        return response['ResponseMetadata']['HTTPStatusCode']
+        ...
+    
 
 
 if(__name__ == '__main__'):
