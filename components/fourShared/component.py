545f4b2af183a934c08165cc4d7c3bf2a10a1166
ryosyntaxx <riodwi12174@gmail.com>
2024-02-11 21:52:53 +0700

    feat: fix

---
+++ components/fourShared/component.py
@@ -0,0 +1,52 @@
+class FourSharedAsset:
+    def __init__(self) -> None:
+        self.cookies = {
+            "hostid": "27532044",
+            "_ga": "GA1.2.1403414382.1707537295",
+            "__utmz": "210074320.1707297936.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)",
+            "day1host": "h",
+            "cd1v": "PlT5",
+            "_gid": "GA1.2.572295155.1707537295",
+            "Login": "1531194522",
+            "Password": "8a85738e0f6d41d3632e4f86a9c08c4e",
+            "ulin": "true",
+            "JSESSIONID": "3570B697DFE1EDEAF579645F36CD71AC.dc571",
+            "4langcookie": "in",
+            "WWW_JSESSIONID": "3570B697DFE1EDEAF579645F36CD71AC.dc571",
+            "dlpvc110439": "N",
+            "__utma": "210074320.1594841001.1707297895.1707300404.1707476997.3",
+            "__utmc": "210074320",
+            "__utmt_gwo": "1",
+            "utrf": "9f5cf7b041",
+            "__utmb": "210074320.2.10.1707476997",
+            "_gat": "1",
+            "_ga_8TB61LMJKC": "GS1.2.1707537296.1.0.1707537296.60.0.0",
+            "_gat_4shared_Clicks": "1",
+            "_ga_6N5M1FBKD4": "GS1.2.1707537297.1.0.1707537297.0.0.0",
+            "justLogged": "1",
+            "showApkOwnerGuidenJ2RwU2o": "null"
+            }
+
+        self.headers = {
+            'Accept': '*/*',
+            'Accept-Language': 'en-US,en;q=0.9,id;q=0.8',
+            'Connection': 'keep-alive',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+            'X-Requested-With': 'XMLHttpRequest',
+            'locale': 'in',
+            'sec-ch-ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'x-security': '1531194522',
+        }
+
+        self.domain = 'www.4shared.com'
+        self.main_url = 'https://www.4shared.com'
+        self.link = 'https://www.4shared.com/office/1VEFZFf8/Islam_Denounces_Terrorism.html#'
+        self.login = 'https://www.4shared.com/web/login?locale=in'
+
+        self.EMAIL = 'swiperhat@tutanota.com'
+        self.PASS = '*Swiper123'
